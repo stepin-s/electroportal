b9898c9b34a3e27c2d19de7c241c4795441b48da
st <stpn.s@yandex.ru>
2020-08-03 10:37:37 +0300

    modified templates

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import News
 
-# Create your views here.
+def news_list(request):
+    news = News.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'portal/news_list.html', {'news': news})
