ea59d786c960b15d78361f228cebdeec7ca042f0
st <stpn.s@yandex.ru>
2020-08-04 10:20:08 +0300

    added view and template for news detail

--- portal/urls.py
+++ portal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.news_list, name='news_list'),
+	path('', views.news_list, name='news_list'),
+	path('new/<int:pk>/', views.new_detail, name='new_detail'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import News
 
 def news_list(request):
     news = News.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'portal/news_list.html', {'news': news})
+
+def new_detail(request, pk):
+    new = get_object_or_404(News, pk=pk)
+    return render(request, 'portal/new_detail.html', {'new': new})
