98ce5fafcc682a877f2a252dd72d7bdffe58b4ad
st <stpn.s@yandex.ru>
2020-08-05 15:09:33 +0300

    added template for edit news and do some css changes

--- portal/urls.py
+++ portal/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.news_list, name='news_list'),
 	path('new/<int:pk>/', views.new_detail, name='new_detail'),
+	path('news/add/', views.new_add, name='new_add'),
+	path('news/<int:pk>/edit/', views.new_edit, name='new_edit'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import News
+from .forms import NewsForm
 
 def news_list(request):
     news = News.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,3 +10,30 @@ def news_list(request):
 def new_detail(request, pk):
     new = get_object_or_404(News, pk=pk)
     return render(request, 'portal/new_detail.html', {'new': new})
+
+def new_add(request):
+    if request.method == "POST":
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            new = form.save(commit=False)
+            new.author = request.user
+            new.published_date = timezone.now()
+            new.save()
+            return redirect('new_detail', pk=new.pk)
+    else:
+        form = NewsForm()
+    return render(request, 'portal/new_edit.html', {'form': form})
+
+def new_edit(request, pk):
+    new = get_object_or_404(News, pk=pk)
+    if request.method == "POST":
+        form = NewsForm(request.POST, instance=new)
+        if form.is_valid():
+            new = form.save(commit=False)
+            new.author = request.user
+            new.published_date = timezone.now()
+            new.save()
+            return redirect('new_detail', pk=new.pk)
+    else:
+        form = NewsForm(instance=new)
+    return render(request, 'portal/new_edit.html', {'form': form})
