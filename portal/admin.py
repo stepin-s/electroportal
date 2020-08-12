ab4c41e5a726a4a91292aeaee968b09ee493f67a
st <stpn.s@yandex.ru>
2020-08-12 15:54:09 +0300

    added view_on_site,reload some icons, improving admin panel

--- portal/admin.py
+++ portal/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import News
 from .models import Videos
 
-admin.site.register(News),
 admin.site.register(Videos)
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title','author', 'created_date', 'published_date')
+
+
+admin.site.register(News, NewsAdmin)
--- portal/models.py
+++ portal/models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
+from django.contrib import admin
+from django.utils.html import format_html
 
 
 class News(models.Model):
@@ -11,12 +14,22 @@ class News(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    class Meta:
+        ordering = ["created_date"]
+        verbose_name_plural = "News"
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('new_detail', args=[str(self.id)])
+
+# class NewsAdmin(admin.ModelAdmin):
+#     list_display = ('author','title','created_date','published_date')
 
 
 class Videos(models.Model):
@@ -24,9 +37,25 @@ class Videos(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     link = models.CharField(max_length=200)
+    GENRE_CHOICES = [
+        ('REV', 'Review'),
+        ('SCI', 'Science'),
+        ('HUM', 'Humor'),
+    ]
+
+    genre = models.CharField(
+        max_length=3,
+        choices=GENRE_CHOICES,
+        default='REV',
+    )
+
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["created_date"]
+        verbose_name_plural = "Videos"
 
     def publish(self):
         self.published_date = timezone.now()
@@ -34,3 +63,6 @@ class Videos(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
